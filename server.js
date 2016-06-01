a1b8bef86c32fd4fba809ddcc23e205296d3d69b
Daniel Noon <supercookies@outlook.com>
2016-06-01 14:19:20 -0500

    debugging node

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 8080;
+
 app.use(express.static('./public'));
 
 app.all("/", function(req, res){
@@ -11,4 +13,4 @@ app.all("/page/*", function(req, res){
   res.sendFile(__dirname + "/public/index.html");
 });
 
-app.listen(8080);
+app.listen(port);
